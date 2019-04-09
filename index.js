1c5efc2c3b524e434f30a896bcaa2d5cea324764
Daniel Ale <denielale9291@gmail.com>
2019-04-09 15:53:30 +0100

    Add Real-time Subscription to Links

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const { GraphQLServer } = require("graphql-yoga");
 const gql = require("graphql-tag");
 const { prisma } = require("./src/generated/prisma-client");
+
 const Query = require("./src/resolvers/Query");
 const Mutation = require("./src/resolvers/Mutation");
 const User = require("./src/resolvers/User");
 const Link = require("./src/resolvers/Link");
+const Subscription = require("./src/resolvers/Subscription");
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
 };
